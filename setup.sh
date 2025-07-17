43adbe3db185b1fc59588bce3738a0f83d957758
Edderic Ugaddan <edderic@gmail.com>
2025-07-17 15:22:39 -0400

    link files

--- setup.sh
+++ setup.sh
@@ -3,4 +3,14 @@
 ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 mkdir -p $HOME/.config/nvim
-ln -s $PWD/init.lua $HOME/.config/nvim/init.lua
+mkdir -p $HOME/.config/nvim/lua
+mkdir -p $HOME/.config/nvim/lua/config
+mkdir -p $HOME/.config/nvim/lua/plugins
+ln -s $PWD/.config/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -s $PWD/.config/nvim/lua/plugins/init.lua $HOME/.config/nvim/lua/plugins/init.lua
+ln -s $PWD/.config/nvim/lua/config/autocmds.lua $HOME/.config/nvim/lua/config/autocmds.lua
+ln -s $PWD/.config/nvim/lua/config/keymaps.lua $HOME/.config/nvim/lua/config/keymaps.lua
+ln -s $PWD/.config/nvim/lua/config/lsp.lua $HOME/.config/nvim/lua/config/lsp.lua
+ln -s $PWD/.config/nvim/lua/config/options.lua $HOME/.config/nvim/lua/config/options.lua
+ln -s $PWD/.config/nvim/lua/config/ruby.lua $HOME/.config/nvim/lua/config/ruby.lua
+ln -s $
